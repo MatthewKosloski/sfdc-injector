a55a56f3ca804904fa317d24e058509bb8054c55
Matthew Kosloski <matthewkosloski@me.com>
2019-05-29 16:40:43 -0500

    created object to hold event fields

---
+++ DataCenterStatusEventFields.cs
@@ -0,0 +1,15 @@
+using System.Runtime.Serialization;
+
+namespace SFDCInjector
+{
+    [DataContract]
+    public class DataCenterStatusEventFields : IPlatformEventFields
+    {
+        
+        [DataMember(Name="Status_Code__c")]
+        public int StatusCode { get; set; }
+
+        [DataMember(Name="Data_Center_Id__c")]
+        public string DataCenterId { get; set; }
+    }
+}
