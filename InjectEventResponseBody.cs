0d08b91f09eebb4f7059434fa8996470b1b16fac
Matthew Kosloski <matthewkosloski@me.com>
2019-05-29 09:45:46 -0500

    created class to hold response data from InjectEvent

---
+++ InjectEventResponseBody.cs
@@ -0,0 +1,18 @@
+using System.Runtime.Serialization;
+
+namespace SFDCInjector
+{
+    [DataContract]
+    public class InjectEventResponseBody {
+
+        [DataMember(Name="id")]
+        public string Id { get; set; }
+
+        [DataMember(Name="success")]
+        public bool Success { get; set; }
+
+        [DataMember(Name="errors")]
+        public string[] Errors { get; set; }
+
+    }
+}
