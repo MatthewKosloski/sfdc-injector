773119a4561aeb1fa001f9460197acccc27d3e1b
Matthew Kosloski <matthewkosloski@me.com>
2019-05-30 12:58:44 -0500

    implemented using statement in SerializerDeserializer

--- SerializerDeserializer.cs
+++ SerializerDeserializer.cs
@@ -6,25 +6,29 @@ namespace SFDCInjector
 {
     public class SerializerDeserializer
     {
+
+        // Deserializes a JSON string into an instance of type T.
         public static T DeserializeJsonToType<T>(string json) where T : class, new()
         {
-            T obj = new T();
-            MemoryStream stream = new MemoryStream(Encoding.UTF8.GetBytes(json));
-            DataContractJsonSerializer serializer = new DataContractJsonSerializer(typeof(T));
-            obj = serializer.ReadObject(stream) as T;
-            stream.Close();
-            return obj;
+            using (MemoryStream stream = new MemoryStream(Encoding.UTF8.GetBytes(json)))
+            {
+                T obj = new T();
+                DataContractJsonSerializer serializer = new DataContractJsonSerializer(typeof(T));
+                obj = serializer.ReadObject(stream) as T;
+                return obj;
+            }
         }
 
         // Serializes an instance of Type T into a JSON string.
         public static string SerializeTypeToJson<T>(T obj)
         {
-            MemoryStream stream = new MemoryStream();  
-            DataContractJsonSerializer serializer = new DataContractJsonSerializer(typeof(T));  
-            serializer.WriteObject(stream, obj);  
-            byte[] json = stream.ToArray();  
-            stream.Close();  
-            return Encoding.UTF8.GetString(json, 0, json.Length);  
+            using (MemoryStream stream = new MemoryStream())
+            {
+                DataContractJsonSerializer serializer = new DataContractJsonSerializer(typeof(T));  
+                serializer.WriteObject(stream, obj);  
+                byte[] json = stream.ToArray();  
+                return Encoding.UTF8.GetString(json, 0, json.Length); 
+            } 
         }
     }
 }
