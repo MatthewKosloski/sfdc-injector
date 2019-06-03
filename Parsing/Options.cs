50a56f2114e4acbe4df1d7eff2d7aaf15bdbbf86
Matthew Kosloski <matthewkosloski@me.com>
2019-06-03 16:37:26 -0500

    added usage example for auth args

--- Parsing/Options.cs
+++ Parsing/Options.cs
@@ -1,14 +1,34 @@
 using CommandLine;
+using CommandLine.Text;
 using System.Collections.Generic;
 
 namespace SFDCInjector.Parsing
 {
     public class Options 
     {
-        [Option('a', "authenticate", Separator=':')]
+        [Option('a', "authenticate", Separator = ':', Required = true, HelpText = "Authenticate with SFDC.")]
         public IEnumerable<string> AuthArgs { get; set; }
 
-        [Option('i', "inject", Separator=':')]
+        [Option('i', "inject", Separator = ':', Required = true, HelpText = "Inject event into SFDC.")]
         public IEnumerable<string> InjectArgs { get; set; }
+
+        [Usage]
+        public static IEnumerable<Example> Examples
+        {
+            get
+            {
+                return new List<Example>() {
+                    new Example("Authenticate with Salesforce via Username-Password Flow", new Options { 
+                        AuthArgs = new List<string> {
+                            "CliendId",
+                            "ClientSecret",
+                            "Username",
+                            "Password",
+                            "ApiVersion"
+                        }
+                    })
+                };
+            }
+        }
     }
 }
