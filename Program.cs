6bf8f4919526993c1d06903f86cee1d5dd83183b
Matthew Kosloski <matthewkosloski@me.com>
2019-06-07 15:00:52 -0500

    instantiate controllers

--- Program.cs
+++ Program.cs
@@ -1,47 +1,20 @@
 ﻿using CommandLine;
 using SFDCInjector.Parsing;
-// using System.Configuration;
-// using SFDCInjector.PlatformEvents.DataCenter;
 
 namespace SFDCInjector
 {
+
     class Program
     {
-
-        // private static SFDCClient CreateSFDCClient() 
-        // {
-        //     var appSettings = ConfigurationManager.AppSettings;
-        //     return new SFDCClient 
-        //     {
-        //         ClientId = appSettings["ClientId"],
-        //         ClientSecret = appSettings["ClientSecret"],
-        //         Username = appSettings["Username"],
-        //         Password = appSettings["Password"],
-        //         ApiVersion = SFDCInjector.Utils.Conversions.StringToDouble(appSettings["ApiVersion"])
-        //     };
-        // }
-
-        // public static void Main(string[] args)
-        // {
-        //     SFDCClient client = CreateSFDCClient();
-        //     client.RequestAccessToken().Wait();
-
-        //     DataCenterStatusEvent statusChangeEvent = new DataCenterStatusEvent {
-        //         Fields = new DataCenterStatusEventFields {
-        //             StatusCode = 404,
-        //             DataCenterId = "a032E00000xzevTQAQ"
-        //         }
-        //     };
-
-        //     client.InjectEvent(statusChangeEvent).Wait();
-        // }
-
-
         static int Main(string[] args) {
+            InjectController injectController = new InjectController();
+            AboutController aboutController = new AboutController();
+
             return Parser.Default.ParseArguments<InjectOptions, AboutOptions>(args).MapResult(
-                (InjectOptions opts) => InjectController.Init(opts),
-                (AboutOptions opts) => AboutController.Init(opts),
+                (InjectOptions opts) => injectController.Init(opts),
+                (AboutOptions opts) => aboutController.Init(opts),
                 errs => 1);
         }
+
     }
 }
