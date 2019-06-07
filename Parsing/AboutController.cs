195e094de52c0d6ba592ea41d91530d9ca770f3a
Matthew Kosloski <matthewkosloski@me.com>
2019-06-07 14:59:25 -0500

    make AboutController a non-static class

--- Parsing/AboutController.cs
+++ Parsing/AboutController.cs
@@ -1,10 +1,11 @@
 using System;
+using SFDCInjector.Parsing;
 
 namespace SFDCInjector.Parsing
 {
-    public static class AboutController
+    public class AboutController
     {
-        public static int Init(AboutOptions opts)
+        public int Init(AboutOptions opts)
         {
             Console.WriteLine("This is a CLI console application built with" + 
             " .Net core that publishes events to Salesforce.");
