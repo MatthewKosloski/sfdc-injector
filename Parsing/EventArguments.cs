acdecae83c2928c102ac85696155c979bae70124
Matthew Kosloski <matthewkosloski@me.com>
2019-06-07 15:00:39 -0500

    implemented EventArguments class in InjectController

---
+++ Parsing/EventArguments.cs
@@ -0,0 +1,25 @@
+using System.Collections.Generic;
+
+namespace SFDCInjector.Parsing
+{
+    public class EventArguments
+    {
+
+        private string _EventClassName;
+        private string _EventFieldsClassName;
+
+        public string EventClassName {
+            get => _EventClassName;
+            set {
+                _EventClassName = value;
+                _EventFieldsClassName = $"{value}Fields";
+            }
+        }
+
+        public string EventFieldsClassName {
+            get => _EventFieldsClassName;
+        }
+
+        public List<object> EventFieldsPropValues { get; set; }
+    }
+}
--- Parsing/InjectController.cs
+++ Parsing/InjectController.cs
@@ -1,24 +1,44 @@
 using System.Configuration; 
 using System.Collections.Generic;
-using System.Diagnostics;
 using System;
 using SFDCInjector.Utils;
 using SFDCInjector.Exceptions;
+using SFDCInjector.Parsing;
 
 namespace SFDCInjector.Parsing
 {
-    public static class InjectController
+    /// <summary>
+    /// Responsible for carrying out initialization actions 
+    /// using the CLI argument values supplied to the `inject` verb.
+    /// </summary>
+    public class InjectController
     {
-        private static AuthenticationArguments SetAuthArgs(IEnumerable<string> cliArgs)
+
+        private AuthenticationArguments _AuthArgs { get; set; }
+        private EventArguments _EventArgs { get; set; }
+        private SFDCClient _Client { get; set; }
+
+        /// <summary>
+        /// Creates a new instance of `AuthenticationArguments`
+        /// using the supplied authentication arguments and the 
+        /// contents of `App.config`.
+        /// <param name="cliAuthArgs">
+        /// An enumerable containing the values supplied to the -a CLI flag.
+        /// </param>
+        /// <exception cref="SFDCInjector.Exceptions.EventInjectionUnsuccessfulException">
+        /// Thrown when App.config is not found and there are missing CLI arguments.
+        /// </exception>
+        /// </summary>
+        private AuthenticationArguments CreateAuthArgs(IEnumerable<string> cliAuthArgs)
         {
             AuthenticationArguments authArgs = new AuthenticationArguments();
             
-            List<string> cliArgsList = new List<string>();
-            foreach(string arg in cliArgs)
-                cliArgsList.Add(arg.Trim());
+            List<string> cliAuthArgsList = new List<string>();
+            foreach(string arg in cliAuthArgs)
+                cliAuthArgsList.Add(arg.Trim());
 
             bool hasNoAppConfig = ConfigurationManager.AppSettings.Count == 0;
-            bool hasMissingCliArgs = Helpers.HasEmptyTrimmedString(cliArgsList);
+            bool hasMissingCliArgs = Helpers.HasEmptyTrimmedString(cliAuthArgsList);
 
             if(hasNoAppConfig && hasMissingCliArgs)
             {
@@ -27,25 +47,74 @@ namespace SFDCInjector.Parsing
             } 
             else
             {
-                authArgs.ClientId = Helpers.KeepOriginalIfEmptyReplacement(authArgs.ClientId, cliArgsList[0]);
-                authArgs.ClientSecret = Helpers.KeepOriginalIfEmptyReplacement(authArgs.ClientSecret, cliArgsList[1]);
-                authArgs.Username = Helpers.KeepOriginalIfEmptyReplacement(authArgs.Username, cliArgsList[2]);
-                authArgs.Password = Helpers.KeepOriginalIfEmptyReplacement(authArgs.Password, cliArgsList[3]);
-                authArgs.ApiVersion = Helpers.KeepOriginalIfEmptyReplacement(authArgs.ApiVersion, cliArgsList[4]);
+                authArgs.ClientId = Helpers.KeepOriginalIfEmptyReplacement(authArgs.ClientId, cliAuthArgsList[0]);
+                authArgs.ClientSecret = Helpers.KeepOriginalIfEmptyReplacement(authArgs.ClientSecret, cliAuthArgsList[1]);
+                authArgs.Username = Helpers.KeepOriginalIfEmptyReplacement(authArgs.Username, cliAuthArgsList[2]);
+                authArgs.Password = Helpers.KeepOriginalIfEmptyReplacement(authArgs.Password, cliAuthArgsList[3]);
+                authArgs.ApiVersion = Helpers.KeepOriginalIfEmptyReplacement(authArgs.ApiVersion, cliAuthArgsList[4]);
             }
 
             return authArgs;
         }
 
-        public static int Init(InjectOptions o)
+        /// <summary>
+        /// Creates a new instance of `EventArguments`
+        /// using the supplied event arguments.
+        /// <param name="cliEvtArgs">An enumerable containing the values supplied to the -e CLI flag.</param>
+        /// </summary>
+        private EventArguments CreateEventArgs(IEnumerable<string> cliEvtArgs)
         {
-            AuthenticationArguments authArgs = SetAuthArgs(o.AuthArgs);
+            EventArguments evtArgs = new EventArguments();
 
-            Console.WriteLine($"ClientId: {authArgs.ClientId}");
-            Console.WriteLine($"ClientSecret: {authArgs.ClientSecret}");
-            Console.WriteLine($"Username: {authArgs.Username}");
-            Console.WriteLine($"Password: {authArgs.Password}");
-            Console.WriteLine($"ApiVersion: {authArgs.ApiVersion}");
+            List<object> cliEvtArgsList = new List<object>();
+
+            foreach(string arg in cliEvtArgs)
+                cliEvtArgsList.Add(arg);
+            
+            evtArgs.EventClassName = (string) cliEvtArgsList[0];
+            
+            // don't include event name in field values
+            cliEvtArgsList.RemoveAt(0);
+
+            evtArgs.EventFieldsPropValues = cliEvtArgsList;
+
+            return evtArgs;
+        }
+
+        /// <summary>
+        /// Creates a new instance of `SFDCClient`
+        /// using the authentication arguments in
+        /// `_AuthArgs` to interact with Salesforce's
+        /// REST API.
+        /// </summary>
+        private SFDCClient CreateClient() 
+        {
+            return new SFDCClient 
+            {
+                ClientId = _AuthArgs.ClientId,
+                ClientSecret = _AuthArgs.ClientSecret,
+                Username = _AuthArgs.Username,
+                Password = _AuthArgs.Password,
+                ApiVersion = _AuthArgs.ApiVersion
+            };
+        }
+
+        /// <summary>
+        /// Injects an event into Salesforce using
+        /// the supplied CLI authentication and 
+        /// event arguments.
+        /// </summary>
+        public int Init(InjectOptions o)
+        {
+            _AuthArgs = CreateAuthArgs(o.AuthArgs);
+            _EventArgs = CreateEventArgs(o.EventArgs);
+            _Client = CreateClient();
+
+            dynamic evt = EventCreator.CreateEvent(_EventArgs.EventClassName, 
+            _EventArgs.EventFieldsClassName, _EventArgs.EventFieldsPropValues);
+
+            _Client.RequestAccessToken().Wait();
+            _Client.InjectEvent(evt).Wait();
 
             return 0;
         }
